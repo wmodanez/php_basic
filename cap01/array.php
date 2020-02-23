be9a3595d6fa69e557eebcd33c93e31ef025823d
Wesley Modanez Freitas <wesley.modanez@gmail.com>
2020-02-23 11:26:56 -0300

    Indentação do código.

--- cap01/array.php
+++ cap01/array.php
@@ -33,23 +33,23 @@ array_push($a, 'roxo');
 $b = array_reverse($a, true);
 
 foreach ($a as $cor) {
-    echo $cor.PHP_EOL;
+    echo $cor . PHP_EOL;
 }
 
 array_shift($a);
 array_pop($a);
 
-echo ''.PHP_EOL;
+echo '' . PHP_EOL;
 foreach ($a as $cor) {
-    echo $cor.PHP_EOL;
+    echo $cor . PHP_EOL;
 }
 
-echo ''.PHP_EOL;
+echo '' . PHP_EOL;
 foreach ($b as $cor) {
-    echo $cor.PHP_EOL;
+    echo $cor . PHP_EOL;
 }
 
-echo ''.PHP_EOL;
+echo '' . PHP_EOL;
 $c = array('verde', 'amarelo');
 $d = array('azul', 'branco');
 $e = array_merge($c, $d);
@@ -57,37 +57,37 @@ print_r($e);
 
 print_r(array_keys($contato));
 print_r(array_values($contato));
-print 'Quantidade: ' .count($contato);
+print 'Quantidade: ' . count($contato);
 
-echo ''.PHP_EOL;
-if(in_array('azul', $e)){
+echo '' . PHP_EOL;
+if (in_array('azul', $e)) {
     echo 'Cor encontada!';
 }
 
 //sort
-echo ''.PHP_EOL;
+echo '' . PHP_EOL;
 sort($e);
 print_r($e);
 
-echo ''.PHP_EOL;
+echo '' . PHP_EOL;
 rsort($e);
 print_r($e);
 
 //asort
-echo ''.PHP_EOL;
+echo '' . PHP_EOL;
 asort($a);
 print_r($a);
 
-echo ''.PHP_EOL;
+echo '' . PHP_EOL;
 arsort($a);
 print_r($a);
 
 //ksort
-echo ''.PHP_EOL;
+echo '' . PHP_EOL;
 ksort($a);
 print_r($a);
 
-echo ''.PHP_EOL;
+echo '' . PHP_EOL;
 krsort($a);
 print_r($a);
 
--- cap01/matriz.php
+++ cap01/matriz.php
@@ -1,14 +1,14 @@
 <?php
 
-$carros = array('City' => array('cor' => 'preto', 'potencia' => '1.5', 'ano'=>'2019'),
-                'Corola' => array('cor' => 'cinza', 'potencia' => '1.8', 'ano'=>'2020'),
-                'Tesla' => array('cor' => 'prata', 'potencia' => '2.1', 'ano'=>'2020'));
+$carros = array('City' => array('cor' => 'preto', 'potencia' => '1.5', 'ano' => '2019'),
+    'Corola' => array('cor' => 'cinza', 'potencia' => '1.8', 'ano' => '2020'),
+    'Tesla' => array('cor' => 'prata', 'potencia' => '2.1', 'ano' => '2020'));
 
-echo $carros['City']['ano'].PHP_EOL;
+echo $carros['City']['ano'] . PHP_EOL;
 
-foreach ($carros as $modelo => $caracteristicas){
+foreach ($carros as $modelo => $caracteristicas) {
     echo "=> modelo $modelo\n";
-    foreach ($caracteristicas as $caracteristica => $valor){
+    foreach ($caracteristicas as $caracteristica => $valor) {
         echo " - caracteristica $caracteristica => $valor\n";
     }
 }
